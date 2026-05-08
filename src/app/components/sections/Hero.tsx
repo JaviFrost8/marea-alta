c0176b2c77e6e58784086167ffd30bf4bb603c96
Javi <Jjaviergonzalez23@gmail.com>
2026-05-08 14:54:58 +0200

    Fix: Nav funcionando correctamente

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -43,8 +43,8 @@ export const Hero = () => {
         className="absolute bottom-10 size-6 animate-bounce"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="m19.5 8.25-7.5 7.5-7.5-7.5"
         />
       </svg>
